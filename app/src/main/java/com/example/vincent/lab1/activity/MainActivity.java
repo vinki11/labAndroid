e37abc438e2e690f4ddaaf977f9eceadb9ac70a7
vinki11 <gelinas.vincent@gmail.com>
2016-09-20 13:15:46 -0400

    fix #1 - Ajout dynamique des fragments

--- app/src/main/java/com/example/vincent/lab1/activity/MainActivity.java
+++ app/src/main/java/com/example/vincent/lab1/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.vincent.lab1.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.vincent.lab1.R;
+import com.example.vincent.lab1.fragment.ListeFragment;
+import com.example.vincent.lab1.fragment.PlaceHolderFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        ListeFragment listeFragment = new ListeFragment();
+        PlaceHolderFragment mapFragment = new PlaceHolderFragment();
+        fragmentTransaction.add(R.id.liste_fragment_container, listeFragment);
+        fragmentTransaction.add(R.id.map_fragment_container, mapFragment);
+        fragmentTransaction.commit();
+
     }
 
 
